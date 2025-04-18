cd64a48e863f6a0081c38cd8d0f4ad12866a58de
Solomia <168734945+Kaena0@users.noreply.github.com>
2025-04-18 10:04:20 +0300

    Update test_prime.py

--- test_prime.py
+++ test_prime.py
@@ -1,5 +1,3 @@
-# test_prime.py
-
 import unittest
 from prime import is_prime
 
@@ -8,16 +6,11 @@ class TestIsPrime(unittest.TestCase):
     def test_primes(self):
         self.assertTrue(is_prime(2))
         self.assertTrue(is_prime(3))
-        self.assertTrue(is_prime(5))
-        self.assertTrue(is_prime(13))
-        self.assertTrue(is_prime(17))
+        self.assertTrue(is_prime(11))
+        self.assertTrue(is_prime(29))
 
     def test_non_primes(self):
         self.assertFalse(is_prime(0))
         self.assertFalse(is_prime(1))
         self.assertFalse(is_prime(4))
-        self.assertFalse(is_prime(9))
         self.assertFalse(is_prime(100))
-
-if __name__ == '__main__':
-    unittest.main()
